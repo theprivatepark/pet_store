5cb647d49318195d6758de426ad861fbaa8193fe
JBondeson19 <jpbond58@gmail.com>
2020-09-15 15:31:37 -0400

    "updated routes and files"

--- pets_store/config/routes.rb
+++ pets_store/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get 'home', to: 'animals#home'
+  get 'home', to: 'static#home'
+  get 'index', to: 'animals#index'
 end
